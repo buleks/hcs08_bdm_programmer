162c9358a42ace548b97bc56e8d4bba8d136fd2b
Mateusz <mateusz@tulip.lol>
2023-12-22 18:26:50 +0100

    read,write functions added, reading bdcsr added

--- hcs08_prog.c
+++ hcs08_prog.c
@@ -31,6 +31,12 @@ pinState get_pin_state(uint8_t pin);
 void enter_background(void);
 void Sync_Command(void);
 
+uint8_t read_BDCSR(void);
+
+void write_BYTE(uint16_t addr,uint8_t data);
+uint8_t read_BYTE(uint16_t addr);
+void write_BYTE_WS(uint16_t addr,uint8_t data);
+
 static FILE out_stream = FDEV_SETUP_STREAM(serial_send, NULL, _FDEV_SETUP_WRITE);
 
 int main(void)
@@ -47,9 +53,456 @@ int main(void)
     printf("\n\rPres enter to generate sync");
     waitEnter();
     Sync_Command();
-    
+    read_BDCSR();
 
     return 0;
+}
+
+#define BIT_CHANGE_DELAY asm("nop");asm("nop");asm("nop");asm("nop");
+#define BIT_STATE_DURATION asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");
+static inline void write_byte(uint8_t byte)
+{
+
+  uint8_t bit = (byte>>(7)) & (0x01); //First MSB bit
+  uint8_t portf_with_bit = 0 ; 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+  //Transmission start
+  DDRF|= 0x01;
+
+  //First bit - MSB - 8 bit
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Second bit - 7 bit
+  bit = (byte>>(6)) & (0x01); 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Third bit - 6bit
+
+    bit = (byte>>(5)) & (0x01); 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Fourth bit - 5bit
+
+    bit = (byte>>(4)) & (0x01); 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Fifth bit - 4 bit
+
+    bit = (byte>>(3)) & (0x01); 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Sixth bit - 3 bit
+
+  bit = (byte>>(2)) & (0x01); 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+  
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Seventh bit - 2 bit
+
+    bit = (byte>>(1)) & (0x01); 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+
+  //Eight bit - 1 bit
+
+  bit = byte&0x01; 
+  if(bit == 0) 
+  { 
+    portf_with_bit =PORTF & ~(0x01);
+  }
+  else 
+  { 
+    portf_with_bit = PORTF | 0x01;
+  }
+
+   PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  
+  PORTF = portf_with_bit;
+ 
+  BIT_STATE_DURATION
+
+  //Bit transmission finished
+  //digitalWrite(BKGD, HIGH);
+  PORTF |= 0x01;
+  //Empty time before starting new bit
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");
+  asm("nop");  
+}
+
+#define BIT_PROBE_DELAY asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");
+#define DELAY_AFTER_BIT_PROBE asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");asm("nop");
+
+static inline uint8_t read_byte(void)
+{
+  uint8_t result =0;
+  uint8_t pin = 0;
+
+  //BKGD as output
+  DDRF|= 0x01;
+
+
+  //First reading bit
+  //BKGD-LOW bit start MSB-7bit ; 
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (7));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE 
+
+  //Second reading bit
+  //BKGD-LOW bit start 6bit ; 
+  DDRF |= 0x01;
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (6));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE 
+
+  //Third reading bit
+  //BKGD-LOW bit start 5bit ; 
+  DDRF |= 0x01;
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (5));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE
+
+  //Fourth reading bit
+  //BKGD-LOW bit start 4bit ; 
+  DDRF |= 0x01;
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (4));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE
+
+  //Fith reading bit
+  //BKGD-LOW bit start 3bit ;
+  DDRF |= 0x01; 
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (3));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE
+
+  //Sixth reading bit
+  //BKGD-LOW bit start 2bit ; 
+  DDRF |= 0x01;
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (2));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE
+
+  //Seventh reading bit
+  //BKGD-LOW bit start 1bit ; 
+  DDRF |= 0x01;
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= (pin << (1));
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE
+
+  //Eight reading bit
+  //BKGD-LOW bit start 0bit ; 
+  DDRF |= 0x01;
+  PORTF &= ~(0x01);
+   //wait 3 BDC clocks, to give time to target to recognise start of transmition
+   //about 0.33us delay
+  BIT_CHANGE_DELAY
+  DDRF&= ~(0x01); //PF0(A0) as input
+  //Probing shoud be 10 BDC clks from bit start
+  //7 nops should give it
+  BIT_PROBE_DELAY
+  pin = PINF&0x01;
+  result |= pin ;
+  //Empty time before starting new bit
+  DELAY_AFTER_BIT_PROBE
+  
+  return result;
+}
+
+#define DLY asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");asm("nop");asm("nop");asm("nop"); \
+        asm("nop");asm("nop");
+        
+uint8_t read_BYTE(uint16_t addr)
+{
+//    noInterrupts();
+   //0xe0 - 1110 0000
+   write_byte(0xe0); //read byte from target memory
+   write_byte((addr&0xff00)>>8); 
+   write_byte(addr&0x00ff); 
+   DLY //SHould be 16 BDCLK cycles
+   uint8_t data = read_byte();
+//    interrupts();
+   return data;
+}
+void write_BYTE(uint16_t addr,uint8_t data)
+{
+//   noInterrupts();
+  write_byte(0xc0); //write byte to target memory 1100 0000
+  write_byte((addr&0xff00)>>8); //0x027F 0000 0010 
+  write_byte(addr&0x00ff); //0111 1111
+  write_byte(data);  // 1010 1010
+  DLY //SHould be 16 BDCLK cycles
+//   interrupts();
+}
+
+void write_BYTE_WS(uint16_t addr,uint8_t data)
+{
+//   noInterrupts();
+  read_BDCSR();
+  write_byte(0xc1); //write byte to target memory
+  write_byte((addr&0xff00)>>8); 
+  write_byte(addr&0x00ff);
+  write_byte(data);  
+  DLY //SHould be 16 BDCLK cycles
+  DLY
+  read_BDCSR();
+//   interrupts();
 }
 
 void Sync_Command(void)
@@ -59,8 +512,6 @@ void Sync_Command(void)
   _delay_us(50); //should take at least 128 BDM cycles of target MCU
   set_BKGD_high();
   configure_BKGD_input();
-  //wait for falling slope
-  uint16_t wait_counter = 0;
   //Sync impulse is schort(less than 2us), therefore this code must be kept simple as is.
   uint8_t timeout1 = 0;
   uint8_t timeout2 = 0;
@@ -175,4 +626,32 @@ int serial_send( char data, FILE *stream)
     while(!(UCSR0A & (1<<UDRE0)));
     UDR0 = data;
     return 0;
+}
+
+uint8_t read_BDCSR(void)
+{
+    //BDC Status and control register
+    //Coding structure
+    //E4/SS
+    // noInterrupts();
+    write_byte(0xe4);//1110-0100
+    uint8_t bdcsr = read_byte();
+    // interrupts();
+    printf("\n\rBDCSR: 0x%x",bdcsr);
+    printf("\n\rENBDM=%d", (bdcsr&0x80)>>7);
+    
+    printf("|BDMACT=%d", (bdcsr&0x40)>>6);
+
+    printf("|BKPTEN=%d", (bdcsr&0x20)>>5);
+
+    printf("|RTS=%d", (bdcsr&0x10)>>4);
+
+    printf("|CLKSW=%d", (bdcsr&0x08)>>3);
+
+    printf("|WS=%d", (bdcsr&0x04)>>2);
+
+    printf("|WSF=%d", (bdcsr&0x02)>>1);
+
+    printf("|DVF=%d", (bdcsr&0x01));
+    return bdcsr;
 }
