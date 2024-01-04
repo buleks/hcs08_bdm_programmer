bfbc90af48c9afb95112ae23aa431ee0fef44610
Mateusz <mateusz@tulip.lol>
2024-01-04 15:09:31 +0100

    Flash functions moved

--- flash.h
+++ flash.h
@@ -1,6 +1,11 @@
 #ifndef __FLASH__
 #define __FLASH__
 
-void test(void);
+void flash_mass_erase(void);
+void flash_write_byte(uint16_t adress, uint8_t data);
+void flash_init(void);
+void flash_print_FSTAT(void);
+void flash_print_FOPT(void);
+void flash_read_print(void);
 
 #endif
